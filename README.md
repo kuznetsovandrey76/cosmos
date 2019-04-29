adc20a3a54d3da3efcc853a0df5a9864d2a9b949
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-04-29 08:30:40 +0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+1. add `env.js` file  
+```
+const env = {
+    HOST: 'YOUR_HOST',
+    USER: 'YOUR_USERNAME',
+    PASSWORD: 'YOUR_PASSWORD',
+    DB: 'YOUR_DATABASE_NAME'
+}
+module.exports.data = env;
+```
---
+++ app.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const fs = require("fs");
+
+const env = require("./env");
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+// connection configurations
+const client = mysql.createConnection({
+    host: `${env.data.HOST}`,
+    user: `${env.data.USER}`,
+    password: `${env.data.PASSWORD}`,
+    database: `${env.data.DB}`
+});
+ 
+// connect to database
+client.connect();
+ 
+// default route
+app.get('/', function (req, res) {
+    console.log(process.env);
+    return res.send({ 
+        error: true, 
+        message: 'hello world' 
+    })
+});
+
+app.get('/data', function (req, res) {
+    client.query('SELECT * FROM tasks', function (error, results, fields) {
+        if (error) throw error;
+
+        // Выводит в консоли
+        // console.log(results); 
+        
+        return res.send({ 
+            // error: false, 
+            data: results, 
+            // message: 'Todos list.' 
+        });
+    });
+});
+
+
+
+
+
+// Retrieve all todos 
+// app.get('/todos', function (req, res) {
+//     client.query('SELECT * FROM tasks', function (error, results, fields) {
+//         if (error) throw error;
+//         console.log(results); // Выводит в консоли
+//         return res.send({ error: false, data: results, message: 'Todos list.' });
+//     });
+// });
+ 
+// Search for todos with ‘bug’ in their name
+// app.get('/todos/search/:keyword', function (req, res) {
+//     let keyword = req.params.keyword;
+//     client.query("SELECT * FROM tasks WHERE task LIKE ? ", ['%' + keyword + '%'], function (error, results, fields) {
+//         if (error) throw error;
+//         return res.send({ error: false, data: results, message: 'Todos search list.' });
+//     });
+// });
+ 
+// Retrieve todo with id 
+// app.get('/todo/:id', function (req, res) {
+ 
+//     let task_id = req.params.id;
+ 
+//     client.query('SELECT * FROM tasks where id=?', task_id, function (error, results, fields) {
+//         if (error) throw error;
+//         return res.send({ error: false, data: results[0], message: 'Todos list.' });
+//     });
+ 
+// });
+ 
+// Add a new todo  
+// app.post('/todo', function (req, res) {
+ 
+//     let task = req.body.task;
+ 
+//     if (!task) {
+//         return res.status(400).send({ error:true, message: 'Please provide task' });
+//     }
+ 
+//     client.query("INSERT INTO tasks SET ? ", { task: task }, function (error, results, fields) {
+//         if (error) throw error;
+//         return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
+//     });
+// });
+ 
+//  Update todo with id
+// app.put('/todo', function (req, res) {
+ 
+//     let task_id = req.body.task_id;
+//     let task = req.body.task;
+ 
+//     if (!task_id || !task) {
+//         return res.status(400).send({ error: task, message: 'Please provide task and task_id' });
+//     }
+ 
+//     client.query("UPDATE tasks SET task = ? WHERE id = ?", [task, task_id], function (error, results, fields) {
+//         if (error) throw error;
+//         return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+//     });
+// });
+ 
+//  Delete todo
+// app.delete('/todo/:id', function (req, res) {
+ 
+//     let task_id = req.params.id;
+ 
+//     client.query('DELETE FROM tasks WHERE id = ?', [task_id], function (error, results, fields) {
+//         if (error) throw error;
+//         return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+//     });
+ 
+// });
+ 
+// all other requests redirect to 404
+app.all("*", function (req, res, next) {
+    return res.send('page not found');
+    next();
+});
+ 
+// port must be set to 3000 because incoming http requests are routed from port 80 to port 8080
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Server start. http://localhost:3000');
+});
+ 
+// allows "grunt dev" to create a development server with livereload
+module.exports = app;
