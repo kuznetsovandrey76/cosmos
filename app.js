cdf79bb88381c21b89418206db68ea05ee6c7068
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-04-30 09:52:59 +0300

    fix lost connection

--- app.js
+++ app.js
@@ -1,154 +1,64 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-
-
 const mysql = require('mysql');
 const env = require("./env");
-
 const pug = require('pug');
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.engine('pug', pug.__express);
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
  
-// connection configurations
-const client = mysql.createConnection({
-    host: `${env.data.HOST}`,
-    user: `${env.data.USER}`,
-    password: `${env.data.PASSWORD}`,
-    database: `${env.data.DB}`
-});
- 
-// connect to database
-// client.connect();
- 
-// default route
-app.get('/', function (req, res) {
-    return res.send({ 
-        error: true, 
-        message: 'hello world' 
-    })
-});
+function reconnect_db(){
+    db = mysql.createConnection({
+        host: `${env.data.HOST}`,
+        user: `${env.data.USER}`,
+        password: `${env.data.PASSWORD}`,
+        database: `${env.data.DB}`
+    });
+    db.on('error', function(err){
+        console.log(err.code);
+		if(err.code == 'PROTOCOL_CONNECTION_LOST') db = reconnect_db();
+		else throw err;
+	});
+	return db;
+}
+var db = reconnect_db();
 
-app.get('/pug', function (req, res) {
-    const index = './views/index.pug'
-    const compiledFunction = pug.compileFile(index);
-    return res.write(compiledFunction());
+let i = 1;
+function func() {
+    console.log(i);
+    i += 1;
+}
+setInterval(func, 1000);
+
+app.get('/', function (req, res) {
+    return res.render('index');
 });
 
 app.post("/add", urlencodedParser, function (req, res) {
     if(!req.body) return res.sendStatus(400);
-    // console.log(req.body.msg);
     let msg = req.body.msg;
 
-    client.query("INSERT INTO tasks SET ? ", { name: msg }, function (error, results, fields) {
-        if (error) throw error;
-        return res.send(`${req.body.msg}`);
+    db.query("INSERT INTO tasks SET ? ", { name: msg }, function (err, results, fields) {
+        if (err) throw err;
+        return res.redirect('/data');
     });
-
-
-
-
-    // res.send(`${req.body.msg}`);
 });
 
 app.get('/data', function (req, res) {
-    client.query('SELECT * FROM tasks', function (error, results, fields) {
-        if (error) throw error;
-
-        // Выводит в консоли
-        // console.log(results); 
-        
-        return res.send({ 
-            // error: false, 
-            data: results, 
-            // message: 'Todos list.' 
-        });
+    db.query('SELECT * FROM tasks', function (err, results, fields) {
+        if (err) throw err;
+        return res.render('data', {results: results});
     });
 });
 
-
-
-
-
-// Retrieve all todos 
-// app.get('/todos', function (req, res) {
-//     client.query('SELECT * FROM tasks', function (error, results, fields) {
-//         if (error) throw error;
-//         console.log(results); // Выводит в консоли
-//         return res.send({ error: false, data: results, message: 'Todos list.' });
-//     });
-// });
- 
-// Search for todos with ‘bug’ in their name
-// app.get('/todos/search/:keyword', function (req, res) {
-//     let keyword = req.params.keyword;
-//     client.query("SELECT * FROM tasks WHERE task LIKE ? ", ['%' + keyword + '%'], function (error, results, fields) {
-//         if (error) throw error;
-//         return res.send({ error: false, data: results, message: 'Todos search list.' });
-//     });
-// });
- 
-// Retrieve todo with id 
-// app.get('/todo/:id', function (req, res) {
- 
-//     let task_id = req.params.id;
- 
-//     client.query('SELECT * FROM tasks where id=?', task_id, function (error, results, fields) {
-//         if (error) throw error;
-//         return res.send({ error: false, data: results[0], message: 'Todos list.' });
-//     });
- 
-// });
- 
-// Add a new todo  
-// app.post('/todo', function (req, res) {
- 
-//     let task = req.body.task;
- 
-//     if (!task) {
-//         return res.status(400).send({ error:true, message: 'Please provide task' });
-//     }
- 
-//     client.query("INSERT INTO tasks SET ? ", { task: task }, function (error, results, fields) {
-//         if (error) throw error;
-//         return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
-//     });
-// });
- 
-//  Update todo with id
-// app.put('/todo', function (req, res) {
- 
-//     let task_id = req.body.task_id;
-//     let task = req.body.task;
- 
-//     if (!task_id || !task) {
-//         return res.status(400).send({ error: task, message: 'Please provide task and task_id' });
-//     }
- 
-//     client.query("UPDATE tasks SET task = ? WHERE id = ?", [task, task_id], function (error, results, fields) {
-//         if (error) throw error;
-//         return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
-//     });
-// });
- 
-//  Delete todo
-// app.delete('/todo/:id', function (req, res) {
- 
-//     let task_id = req.params.id;
- 
-//     client.query('DELETE FROM tasks WHERE id = ?', [task_id], function (error, results, fields) {
-//         if (error) throw error;
-//         return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
-//     });
- 
-// });
- 
 // all other requests redirect to 404
 app.all("*", function (req, res, next) {
     return res.send('page not found');
@@ -159,6 +69,3 @@ app.all("*", function (req, res, next) {
 app.listen(process.env.PORT || 3000, function () {
     console.log('Server start. http://localhost:3000');
 });
- 
-// allows "grunt dev" to create a development server with livereload
-module.exports = app;
